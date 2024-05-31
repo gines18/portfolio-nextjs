3212604877a789261814461d3a3a12dafd03c753
Mariusz Ruczkowski <mario@Mariuszs-Mini.lan>
2024-05-31 19:13:14 +0100

    add Clients component with InifiniteMovingCards

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { FloatingNav } from '@/components/ui/FloatingNavbar';
 import { FaHome } from "react-icons/fa";
 import RecentProjects from '@/components/RecentProjects'
 import {navItems} from '@/data/index'
+import Clients from '@/components/Clients';
 export default function Home() {
   return (
    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
@@ -12,6 +13,7 @@ export default function Home() {
         <Hero />
         <Grid />
         <RecentProjects />
+        <Clients />
     </div>
    </main>
   );
