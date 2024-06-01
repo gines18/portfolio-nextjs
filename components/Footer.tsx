74e5462f12e86eec531ed501bc286d4b42c20572
Mariusz Ruczkowski <mario@Mariuszs-Mini.lan>
2024-06-01 14:29:09 +0100

    fix navbar items not showing on small devices

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,20 +5,14 @@ import { socialMedia } from "@/data";
 import { div } from "three/examples/jsm/nodes/Nodes.js";
 const Footer = () => {
   return (
-    <footer className="w-full pt-20 pb-10" id="contact">
-      <div className="w-full absolute left-0 -bottom-72 min-h-96">
-        <img
-          src="./footer-grid.svg"
-          alt="grid"
-          className="w-full h-full opacity-50"
-        />
-      </div>
+    <footer className="w-full" id="contact">
+  
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
           Ready to take <span className="text-purple">your</span> digital
           presence to the next level?
         </h1>
-        <p className="text-white-200 md:mt-10 my-5 text-center">
+        <p className="text-white-200 md:mt-10 m-5 my-5 p-2 text-center">
           Reach out to me today and let&apos;s discuss how I can help to achieve
           your goals{" "}
         </p>
