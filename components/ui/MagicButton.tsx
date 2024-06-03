1118756ae7f8d79fac301830c12c25cdc1107b0c
Mariusz Ruczkowski <mario@Mariuszs-Mini.lan>
2024-06-03 11:32:59 +0100

    fix MagicButton issues by removing otherClasses from the object

--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -5,13 +5,13 @@ const MagicButton = ({
   icon,
   position,
   handleClick,
-  otherClasses,
+
 }: {
   title: string;
   icon: React.ReactNode;
   position: string;
   handleClick?: () => void;
-  otherClasses?: string;
+
 }) => {
   return (
     <button className="relative inline-flex h-12 w-full md:w-60 md:mt-10 overflow-hidden rounded-lg p-[1px] focus:outline-none" onClick={handleClick}>
@@ -19,7 +19,7 @@ const MagicButton = ({
 
       <span
         className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg
-           bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2 ${otherClasses}`}
+           bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2`}
       >
         {position === "left" && icon}
         {title}
