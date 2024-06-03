cb51aa867d6c34f08ff5cad19b4c44f361414fe6
Mariusz Ruczkowski <mario@Mariuszs-Mini.lan>
2024-06-03 12:33:57 +0100

    replace MagicButton with TopGradient

--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -14,10 +14,9 @@ const MagicButton = ({
 
 }) => {
   return (
-    <button className="relative inline-flex h-12 w-full md:w-60 md:mt-10 overflow-hidden rounded-lg p-[1px] focus:outline-none" onClick={handleClick}>
-      <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-
-      <span
+    <button className="px-8 py-2 rounded-full relative text-white text-sm hover:shadow-2xl hover:shadow-white/[0.1] transition duration-200 border border-slate-600" onClick={handleClick}>
+        <div className="absolute inset-x-0 h-px w-1/2 mx-auto -top-px shadow-2xl  bg-gradient-to-r from-transparent via-teal-500 to-transparent" />
+         <span
         className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg
            bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2`}
       >
@@ -25,8 +24,23 @@ const MagicButton = ({
         {title}
         {position === "right" && icon}
       </span>
-    </button>
+      </button>
   );
 };
 
 export default MagicButton;
+
+
+
+{/* <button className="px-8 py-2 rounded-full relative bg-slate-700 text-white text-sm hover:shadow-2xl hover:shadow-white/[0.1] transition duration-200 border border-slate-600" onClick={handleClick}>
+        <div className="absolute inset-x-0 h-px w-1/2 mx-auto -top-px shadow-2xl  bg-gradient-to-r from-transparent via-teal-500 to-transparent" />
+         <span
+        className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg
+           bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2`}
+      >
+        {position === "left" && icon}
+        {title}
+        {position === "right" && icon}
+      </span>
+      </button>
+  */}
