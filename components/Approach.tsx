739e8ee23ee4d73f4a3341437cb3919b762db0f7
Mariusz Ruczkowski <mario@Mariuszs-Mini.lan>
2024-06-03 12:52:20 +0100

    change button in Approach component

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -109,15 +109,37 @@ const Card = ({
 const AceternityIcon = ({ order }: { order: string }) => {
   return (
     <div>
-      <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-        <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-        <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-6 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-          {order}
-        </span>
+   <button className="px-8 py-2 rounded-full relative bg-slate-700 text-white text-sm hover:shadow-2xl hover:shadow-white/[0.1] transition duration-200 border border-slate-600">
+        <div className="absolute inset-x-0 h-px w-1/2 mx-auto -top-px shadow-2xl  bg-gradient-to-r from-transparent via-teal-500 to-transparent" />
+         <span
+        className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg
+            px-7 text-sm font-medium  gap-2`}
+      >
+        
+        {order}
+        
+      </span>
       </button>
     </div>
   );
 };
+
+{/* <button className="px-8 py-2 rounded-full relative bg-slate-700 text-white text-sm hover:shadow-2xl hover:shadow-white/[0.1] transition duration-200 border border-slate-600" onClick={handleClick}>
+        <div className="absolute inset-x-0 h-px w-1/2 mx-auto -top-px shadow-2xl  bg-gradient-to-r from-transparent via-teal-500 to-transparent" />
+         <span
+        className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg
+           bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2`}
+      >
+        {position === "left" && icon}
+        {title}
+        {position === "right" && icon}
+      </span>
+      </button>
+  */}
+
+
+
+
 
 export const Icon = ({ className, ...rest }: any) => {
   return (
--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -18,7 +18,7 @@ const MagicButton = ({
         <div className="absolute inset-x-0 h-px w-1/2 mx-auto -top-px shadow-2xl  bg-gradient-to-r from-transparent via-teal-500 to-transparent" />
          <span
         className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg
-           bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2`}
+            px-7 text-sm font-medium gap-2`}
       >
         {position === "left" && icon}
         {title}
